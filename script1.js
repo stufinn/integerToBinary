287432b390ca0cc0a5f5e0e30aeb93540fef865e
Stu Finn <stu@borealisweb.ca>
2023-11-09 18:28:28 -0500

    Initial styling

---
+++ script1.js
@@ -0,0 +1,6 @@
+const integer = 25;
+const original_num = document.getElementById('num');
+original_num.innerHTML = integer;
+
+const result_num = document.getElementById('result');
+result_num.innerHTML = integer.toString(2);

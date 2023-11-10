135f445e7c96ea31111a164b5ad7b78c3bb706f4
Stu Finn <stu@borealisweb.ca>
2023-11-09 19:04:25 -0500

    Everything but fart button

--- script1.js
+++ script1.js
@@ -1,6 +1,26 @@
-const integer = 25;
-const original_num = document.getElementById('num');
-original_num.innerHTML = integer;
+// const integer = 25;
+// const original_num = document.getElementById('num');
+// original_num.innerHTML = integer;
 
-const result_num = document.getElementById('result');
-result_num.innerHTML = integer.toString(2);
+// const result_num = document.getElementById('result');
+// result_num.innerHTML = integer.toString(2);
+const input = '42';
+const isInteger = Number.isInteger(parseInt(input));
+console.log(isInteger); // true
+
+function convertToBinary() {
+  const int = document.getElementById('number').value;
+  const isInteger = Number.isInteger(parseFloat(int));
+  if (isInteger === true) {
+    const result = parseInt(int, 10).toString(2);
+    const original_num = document.getElementById('num');
+
+    original_num.innerHTML = int;
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = result;
+  } else {
+    alert('Enter an integer please.');
+  }
+
+  return;
+}
